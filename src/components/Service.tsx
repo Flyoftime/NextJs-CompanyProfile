b3f522c0e45f8e992bc2c3c280655b75d2e381c6
Flyoftime <mirsabanwar@gmail.com>
2024-02-10 23:24:46 +0700

    card

--- src/components/Service.tsx
+++ src/components/Service.tsx
@@ -2,36 +2,37 @@ import React from 'react'
 import "./style.css"
 const Service = () => {
     return (
-<<<<<<< HEAD
-        
-        
-=======
-         
         <div className="bg-[#ffff]  " >
-            <div>
-            <span className="text-[#c76a1a] text-[#15px] bg-[#c2fcf0] font-sans">What We Do</span>
-            <h2 className='text-[#181C28] font-sans text-[37px]'>Our <span className="text-primary text-[37px] text-[#c76a1a] font-sans">Services</span></h2>
-            <p className='text-[#181C28] font-sans text-[16px]'>This is Photoshop version  of Lorem Ipsum. Proin gravida nibh vel <br />velit auctor aliquet. Aenean sollicitudin.</p>
+        <div className='flex justify-center'>
+            <span className='text-[#c76a1a] text-[15px] bg-[#c2fcf0] m-2 p-[15px]'>What We Do</span>
+            <h2 className='text-[#181c28] font-sans text-[36px] m-[15px] p-[30px]'>Our <span className='text-[#c76a1a] font-sans text-[36px]'>Services</span></h2>
+            <p className='text-[#181c28] font-sans text-[16px]'>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
+        </div>
+        <div className='m-o p-0 flex justify-center align-items-center h-[100vh] bg-[#1111] '>
+            <div className='crd'>
+                <div className='ovrlay'>
+                    <h1 className='font-sans text-[#181c28] text-[24px] m-[12px]'>Digital Branding</h1>
+                    <p className='font-sans text-[#181c28] text-[16px]'>This is Photoshop version  of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin.</p>
+                    <a href="#" className='btn'>Get Started</a>
+                </div>
             </div>
-            <div className='flex min-h-screen items-center justify-center bg-neutral-900'>
-                <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
-                    <div className='group relative items-center justify-center overflow-hidden cursor-pointer'>
-                        <div className='h-96 w-72'>
-                            <img className='h-full w-full object-cover' src="/assets/Go Younjung.jpg" alt="" />
-                        </div>
-                        <div className='absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-black'></div>
-                        <div className='absolute inset-0 flex flex-col items-center justify-center px-9 text-center translate-y-[60%] group-hover:translate-y-0 transition-all'>
-                            <h1 className='text-3xl font-bold text-white pb-6'>Digital Branding</h1>
-                            <p className='text-lg italic text-white mb-3'>Lorem Ipsum dolor sit amet consectetur adipiscing elit.</p>
-                            <button className='rounded-fil bg-neutral-900 py-2 px-3.5 text-sm capitalize text-white'> 
-                            See More
-                            </button>
-                        </div>
-                    </div>
+            <div className='crd'>
+                <div className='ovrlay'>
+                    <h1 className='font-sans text-[#181c28] text-[24px] m-[12px]'>Seo Optimization</h1>
+                    <p className='font-sans text-[#181c28] text-[16px]'>This is Photoshop version  of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin.</p>
+                    <a href="#" className='btn'>Get Started</a>
+                </div>
+            </div>
+            <div className='crd'>
+                <div className='ovrlay'>
+                    <h1 className='font-sans text-[#181c28] text-[24px] m-[12px]'>Wireframe Design</h1>
+                    <p className='font-sans text-[#181c28] text-[16px]'>This is Photoshop version  of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin.</p>
+                    <a href="#" className='btn'>Get Started</a>
                 </div>
             </div>
         </div>
->>>>>>> cd995fa29f4fab0807e7db07a78ac96fc3a98e93
+    </div>
+        
       
     )
 }
