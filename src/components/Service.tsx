2a44cb761d3ab17349d5822972a5439c8e587b57
Flyoftime <mirsabanwar@gmail.com>
2024-02-10 23:22:09 +0700

    q

--- src/components/Service.tsx
+++ src/components/Service.tsx
@@ -1,32 +1,9 @@
 import React from 'react'
-
+import "./style.css"
 const Service = () => {
     return (
-         
-        <div className="bg-[#ffff]  " >
-            <div>
-            <span className="text-[#c76a1a] text-[#15px] bg-[#c2fcf0] font-sans">What We Do</span>
-            <h2 className='text-[#181C28] font-sans text-[37px]'>Our <span className="text-primary text-[37px] text-[#c76a1a] font-sans">Services</span></h2>
-            <p className='text-[#181C28] font-sans text-[16px]'>This is Photoshop version  of Lorem Ipsum. Proin gravida nibh vel <br />velit auctor aliquet. Aenean sollicitudin.</p>
-            </div>
-            <div className='flex min-h-screen items-center justify-center bg-neutral-900'>
-                <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
-                    <div className='group relative items-center justify-center overflow-hidden cursor-pointer'>
-                        <div className='h-96 w-72'>
-                            <img className='h-full w-full object-cover' src="/assets/Go Younjung.jpg" alt="" />
-                        </div>
-                        <div className='absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-black'></div>
-                        <div className='absolute inset-0 flex flex-col items-center justify-center px-9 text-center translate-y-[60%] group-hover:translate-y-0 transition-all'>
-                            <h1 className='text-3xl font-bold text-white'>Digital Branding</h1>
-                            <p className='text-lg italic text-white mb-3'>Lorem Ipsum dolor sit amet consectetur adipiscing elit.</p>
-                            <button className='rounded-fil bg-neutral-900 py-2 px-3.5 text-sm capitalize text-white'> 
-                            See More
-                            </button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+        
+        
       
     )
 }
