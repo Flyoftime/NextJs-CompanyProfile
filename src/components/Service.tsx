237ea4a0797bf2af6bd2d15f290fc3bd1b224180
lulzxz <zakyarifrahman83@gmail.com>
2024-02-11 00:24:26 +0700

    service

--- src/components/Service.tsx
+++ src/components/Service.tsx
@@ -2,12 +2,10 @@ import React from 'react'
 import "./style.css"
 const Service = () => {
     return (
-        <div className="bg-[#ffff]" >
+        <div className="bg-[#ffff]  " >
         <div className='p-8 row flex flex-wrap items-center w-full'>
         <div className='col-lg-6 col-start-12 mx-auto mb-5 text-center p-8'>
             <span className='text-[#c76a1a] text-[15px] bg-[#c2fcf0] m-2 p-[15px] font-bold'>What We Do</span>
-        <div className='text-center'>
-            <span className='text-[#c76a1a] text-[15px] bg-[#c2fcf0] m-2 p-[15px]'>What We Do</span>
             <h2 className='text-[#181c28] font-sans text-[36px] m-[15px] p-[30px]'>Our <span className='text-[#c76a1a] font-sans text-[36px]'>Services</span></h2>
             <p className='text-[#181c28] font-sans text-[16px]'>This is Photoshop version of Lorem Ipsum. </p>
             <p className='text-[#181c28] font-sans text-[16px]'> Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin.</p>
@@ -17,10 +15,6 @@ const Service = () => {
             <div className='crd rounded-2xl'>
                 <div className='ovrlay '>
                     <h1 className='font-sans text-[#181c28] text-[24px] m-[12px] '>Digital Branding</h1>
-        <div className='m-o p-0 flex justify-center align-items-center h-[100vh] bg-[#fff] '>
-            <div className='crd'>
-                <div className='ovrlay'>
-                    <h1 className='font-sans text-[#181c28] text-[24px] m-[12px]'>Digital Branding</h1>
                     <p className='font-sans text-[#181c28] text-[16px]'>This is Photoshop version  of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin.</p>
                     <a href="#" className='btn'>Get Started</a>
                 </div>
@@ -41,10 +35,7 @@ const Service = () => {
             </div>
         </div>
     </div>
-        </div>
-        </div>
-        </div>
-        </div>
+        
       
     )
 }
