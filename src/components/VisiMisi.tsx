97485427ddbc924eb5c86e83fa1107310f2fff21
Flyoftime <mirsabanwar@gmail.com>
2024-02-19 11:51:30 +0700

    dah

--- src/components/VisiMisi.tsx
+++ src/components/VisiMisi.tsx
@@ -5,57 +5,55 @@ const VisiMisi = () => {
         <div className='p-20 m-auto'>
             <h1 className='text-3xl font-bold pl-40 pt-4 text-left '>Visi Misi</h1>
             <div className='grid grid-cols-1 divide-y divide-slate-400 divide-solid  pt-4 '>
-            <div>
-            </div>
-            <div>
-                <h3 className='font-bold text-2xl pt-4 pl-40'>Visi</h3>
-                <p className='pl-8 pt-2 text-justify '>
-                    PT. INDORUB adalah perusahaan yang bergerak dalam bidang produksi karet yang berkualitas,
-                    berupaya menjadi perusahaan yang <p>berskala nasional dan internasional,
-                    berdaya saing, serta senantiasa mengutamakan kepuasan pelanggan.</p>
-                </p>
-                <div className='pt-5 '>
-                    <h3 className='font-bold text-2xl pt-4 pl-40 '>Misi</h3>
-                    <div className='pt-2 pl-8 text-justify'>
-                        <li>Meningkatkan mutu hasil produksi sesuai kebutuhan dan keinginan pelanggan</li>
-                        <li>Meningkatkan kualitas sumber days manusia melalui peningkatan pengetahuan, keterampilan dan kreatifitas</li>
-                        <li>Mengarahkan clan mengendalikan organisasi sesuai dengan persyaratan standar internasional ISO 9001:2000</li>
-                        <li>Meningkatkan kualitas sumber days manusia melalui peningkatan pengetahuan, keterampilan dan kreatifitas</li>
-                        <li>Meningkatkan volume penjualan (market share) melalui kegiatan pemasaran yang lebih proaktif dan optimal</li>
-                        <li>Berusaha memperbaiki tingkat kegagalan produk seminimal mungkin atau zero deffect</li>
-                        <li>Meningkatakan efisiensi dan produktifitas di semua lini perusahaan</li>
-                        <li>Membangun cooperative networking dengan pihak pelanggan, pemasok & subkontraktors</li>
-                        <li>Menciptakan lingkungan kerja yang kondusif</li>
-                    </div>
+                <div>
                 </div>
-                <div className='pt-5'>
-                    <h3 className='font-bold text-2xl pt-4 pl-40 pb-2'>Core Relief</h3>
-                    <p className='text-justify'>Core Belief PT. INDORUB adalah merupakan kepercayaan yang memberikan landasan kepada semua jajaran staf dan karyawan tentang hakikat</p>
-                     
-                     <p className='text-justify'>dan makna dari kehidupan manusia Core Belief PT.
-                        INDORUB adalah gerak langkah dan perilaku dalam bekerja seluruhnya kami niatkan secara </p>
-                        
-                        <p className='text-justify'>lulus dan ikhlas bahwa itu adalah ibadah,
-                        disertai ikhtiar untuk mencapai kesuksesan dan manfaat bagi seluruh Stake Holders PT. INDORUB,<br></br> kami percaya jika prestasi kits baik, 
-                        pasti ada imbalan yang setimpal dari prestasi yang baik.</p>
-                </div>
-                <div className='pt-5'>
-                    <h3 className='font-bold text-2xl pt-4 pl-40 pb-2'>Core Values</h3>
-                    <p className='text-justify '>Core Values adalah nilai-nilai budaya kerja yang dimiliki dan dikernbangkan oleh semua insan PT. 
-                        INDORUB yang diturunkan dari satu generasi ke generasi berikutnya.
+                <div>
+                    <h3 className='font-bold text-2xl pt-4 pl-40'>Visi</h3>
+                    <p className='pl-8 pt-2 text-justify '>
+                        PT. INDORUB adalah perusahaan yang bergerak dalam bidang produksi karet yang berkualitas,
+                        berupaya menjadi perusahaan yang berskala nasional dan internasional,
+                        berdaya saing, serta senantiasa mengutamakan kepuasan pelanggan.
                     </p>
-                    <div className='pt-2'>
-                        <p className='pb-2'>Core Values PT.INDORUB adalah:</p>
-                        <li className='pl-6'>KEPERCAYAAN Membangun kepercayaan, kejujuran, etika dan sikap yang terpuji</li>
-                        <li className='pl-6'>KEPERCAYAAN Membangun kepercayaan, kejujuran, etika dan sikap yang terpuji</li>
-                        <li className='pl-6'>KREATIFITAS Menciptakan ide-ide kreatifitas dan inovasi untuk meningkatkan efisiensi dan produktifitas</li>
-                        <li className='pl-6'>KOMITMEN Memenuhi semua komitmen dalam berbisnis untuk menjaga keunggulan dalam persaingan</li>
-                        <li className='pl-6'>KOOPERATIF Selalu bekerja sama secara maksimal dan keikhlasan membantu pihak lain yang membutuhkan, komunikasi yang baik, ramah,<p className='pl-6'>senyum, sopan dan simpatik</p></li>
+                    <div className='pt-5 '>
+                        <h3 className='font-bold text-2xl pt-4 pl-40 '>Misi</h3>
+                        <div className='pt-2 pl-8 text-justify'>
+                            <li>Meningkatkan mutu hasil produksi sesuai kebutuhan dan keinginan pelanggan</li>
+                            <li>Meningkatkan kualitas sumber days manusia melalui peningkatan pengetahuan, keterampilan dan kreatifitas</li>
+                            <li>Mengarahkan clan mengendalikan organisasi sesuai dengan persyaratan standar internasional ISO 9001:2000</li>
+                            <li>Meningkatkan kualitas sumber days manusia melalui peningkatan pengetahuan, keterampilan dan kreatifitas</li>
+                            <li>Meningkatkan volume penjualan (market share) melalui kegiatan pemasaran yang lebih proaktif dan optimal</li>
+                            <li>Berusaha memperbaiki tingkat kegagalan produk seminimal mungkin atau zero deffect</li>
+                            <li>Meningkatakan efisiensi dan produktifitas di semua lini perusahaan</li>
+                            <li>Membangun cooperative networking dengan pihak pelanggan, pemasok & subkontraktors</li>
+                            <li>Menciptakan lingkungan kerja yang kondusif</li>
+                        </div>
+                    </div>
+                    <div className='pt-5'>
+                        <h3 className='font-bold text-2xl pt-4 pl-40 pb-2'>Core Relief</h3>
+                        <p className='text-justify'>Core Belief PT. INDORUB adalah merupakan kepercayaan yang memberikan landasan kepada semua jajaran staf dan karyawan tentang hakikat</p>
+                        <p className='text-justify'>dan makna dari kehidupan manusia Core Belief PT.
+                            INDORUB adalah gerak langkah dan perilaku dalam bekerja seluruhnya kami niatkan secara </p>
+                        <p className='text-justify'>lulus dan ikhlas bahwa itu adalah ibadah,
+                            disertai ikhtiar untuk mencapai kesuksesan dan manfaat bagi seluruh Stake Holders PT. INDORUB,<br></br> kami percaya jika prestasi kits baik,
+                            pasti ada imbalan yang setimpal dari prestasi yang baik.</p>
+                    </div>
+                    <div className='pt-5'>
+                        <h3 className='font-bold text-2xl pt-4 pl-40 pb-2'>Core Values</h3>
+                        <p className='text-justify '>Core Values adalah nilai-nilai budaya kerja yang dimiliki dan dikernbangkan oleh semua insan PT.
+                            INDORUB yang diturunkan dari satu generasi ke generasi berikutnya.
+                        </p>
+                        <div className='pt-2'>
+                            <p className='pb-2'>Core Values PT.INDORUB adalah:</p>
+                            <li className='pl-6'>KEPERCAYAAN Membangun kepercayaan, kejujuran, etika dan sikap yang terpuji</li>
+                            <li className='pl-6'>KEPERCAYAAN Membangun kepercayaan, kejujuran, etika dan sikap yang terpuji</li>
+                            <li className='pl-6'>KREATIFITAS Menciptakan ide-ide kreatifitas dan inovasi untuk meningkatkan efisiensi dan produktifitas</li>
+                            <li className='pl-6'>KOMITMEN Memenuhi semua komitmen dalam berbisnis untuk menjaga keunggulan dalam persaingan</li>
+                            <li className='pl-6'>KOOPERATIF Selalu bekerja sama secara maksimal dan keikhlasan membantu pihak lain yang membutuhkan, komunikasi yang baik, ramah,<p className='pl-6'>senyum, sopan dan simpatik</p></li>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
     )
 }
 
